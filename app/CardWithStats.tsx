64c985ce1dadb6e41e8dc3e665c0ddc14e1d6a37
unknown <krishna.s.mandge@gmail.com>
2024-02-28 23:26:37 +0530

    Adding Follow button and make it work

--- app/CardWithStats.tsx
+++ app/CardWithStats.tsx
@@ -1,20 +1,22 @@
 import { Card, Image, Text, Group, Badge, Center, Button } from '@mantine/core';
 import { IconWebhook, IconMail,IconPhone,IconUser,IconTrash } from '@tabler/icons-react';
 import classes from './Css/CardWithStats.module.css';
+import ChangeFollow from './ChangeFollow'
 import exp from 'constants';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { url } from 'inspector';
 
 
 
 export function CardWithStats(props) {
  
- let Myfun = function(){
-
- }
+ 
 
  return(
-  props.Data.map((data)=>(
+  props.Data.map((data)=>
+  (
+    
+
     <Card withBorder radius="md" className={classes.card} style={{width:'500px', display:'inline-block',margin:'20px'}}>
         <Card.Section className={classes.imageSection}>
              <Image style={{height:'200px', borderRadius:'100px'}} src={`https://api.dicebear.com/7.x/initials/svg?seed=${data.name}`} alt="Name here" />
@@ -41,8 +43,10 @@ export function CardWithStats(props) {
     </Center>
 
     <Group justify="center">
-    <Button style={{width:'40%'}}><IconUser></IconUser>Follow</Button>
+    <ChangeFollow isfollow={data.isfollow}>Follow</ChangeFollow>
+
     <Button variant='default' style={{width:'40%', margin:'10px'}}><IconTrash></IconTrash>Delete</Button>
+      <h1>{data.isfollow=='0'?'default':'none'}</h1>
       </Group>
 
    
---
+++ app/ChangeFollow.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@mantine/core";
+import { useRef, useState } from "react";
+
+function Button1(prop){
+
+   let  [fol,chan] = useState(prop.isfollow)
+    return(
+        <>
+        <Button  variant={fol==0?'none':'default'}
+        onClick={()=>fol=='0'?chan('1'):chan('0')}
+        style={{width:'40%'}}>{fol==0?'Follow':'Unfollow'}</Button>
+
+           </>
+    )
+}
+
+export default Button1;
